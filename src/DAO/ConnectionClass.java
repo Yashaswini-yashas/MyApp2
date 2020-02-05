d7bda037d42917d48fea8196fa1bdd58c166fecd
US1E007 <55137881+US1E007@users.noreply.github.com>
2020-02-05 14:38:54 +0530

    Update ConnectionClass.java

--- src/DAO/ConnectionClass.java
+++ src/DAO/ConnectionClass.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class ConnectionClass {
 	public static Connection intializeConn() throws ClassNotFoundException, SQLException {
-		String name = "uqn9VQ6Hqy";
-		String username = "uqn9VQ6Hqy";
-		String password = "vooaYhKWmG";
-		String dbURL = "jdbc:mysql://remotemysql.com:3306/uqn9VQ6Hqy"; 
+		String name = "5V7ccDnOPH";
+		String username = "5V7ccDnOPH";
+		String password = "EiSIkp2q3j";
+		String dbURL = "jdbc:mysql://remotemysql.com:3306/5V7ccDnOPH"; 
 	    Class.forName("com.mysql.cj.jdbc.Driver");
 	    Connection con = DriverManager.getConnection(dbURL, username, password);
 		return con;
