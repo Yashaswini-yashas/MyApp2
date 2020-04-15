70ee8f19fff53121a09a5236deeff79eb94f0098
US1E007 <55137881+US1E007@users.noreply.github.com>
2020-04-15 14:49:55 +0530

    Update ReadServlet.java

--- src/Controller/ReadServlet.java
+++ src/Controller/ReadServlet.java
@@ -18,13 +18,7 @@ import DAO.PlayersDAOImpl;
 
 public class ReadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static String capitalize(String str) {
-	    if(str == null || str.isEmpty()) {
-	        return str;
-	    }
-
-
-	public static String capitalize(String str) {
+          public static String capitalize(String str) {
 		if (str == null || str.isEmpty()) {
 			return str;
 		}
